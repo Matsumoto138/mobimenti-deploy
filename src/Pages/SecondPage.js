a94f486f6120ed50dab8a74478f996f574e72fc0
Fatih Demirbas <dmrbs.fd@gmail.com>
2022-11-07 22:25:08 +0300

    added animations

--- src/Pages/SecondPage.js
+++ src/Pages/SecondPage.js
@@ -1,31 +1,70 @@
 import React from 'react'
+import { useState, useRef, useEffect } from 'react';
 import '../Style/SecondPage.css'
 import PartOneImg from '../Images/mentoring1.png'
 import PartTwoImg from "../Images/mentoring3.png";
 
 function SecondPage() {
+    const targetRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false)
+
+    const callbackFunction = entries =>{
+        const [entry] = entries
+        setIsVisible(entry.isIntersecting)
+        if (isVisible) {
+            document.getElementById("secondpage-partOneText").classList.add("active")
+            document.getElementById("secondpage-partOneImg").classList.add("active")
+            document.getElementById("secondpage-partTwoImg").classList.add("active")
+            document.getElementById("secondpage-partTwoText").classList.add("active")
+            console.log(entry);
+        }
+    }
+
+    const options = {
+        root:null,
+        rootMargin:"0px",
+        threshold:0.5
+    }
+
+    useEffect(() => {
+      
+        const observer = new IntersectionObserver(callbackFunction, options)
+        if (targetRef.current) {
+            observer.observe(targetRef.current)
+        }
+      return () => {
+        if (targetRef.current) {
+            observer.unobserve(targetRef.current)
+        }
+      }
+    }, [targetRef, options])
+    
+
+    
   return (
-    <div className='SecondPage'>
-        <div className="part-one">
-            <div className="text">
+    
+    <div className='SecondPage' ref={targetRef} >
+        <div className="part-one"  >
+            <div className="text" id='secondpage-partOneText' >
                 <p>Bilgi ve Deneyimlerinle Birçok Gencin Hayatına Dokunabilirsin</p>
             </div>
-            <div className="img">
+            <div className="img" id='secondpage-partOneImg' >
                 <img src={PartOneImg} alt="" />
             </div>
         </div>
         <div className="part-two">
-            <div className="img">
+            <div className="img" id='secondpage-partTwoImg'>
                 <img src={PartTwoImg} alt="" />
             </div>
 
-            <div className="text">
+            <div className="text" id='secondpage-partTwoText'>
                 <p>Bilmediklerini Keşfetmek, Mentor Olmanın Sana Sunduğu Bir Hediye Olurken
                     Bildiklerini Hatırlamak da Bu SÜrecin Sana Başka Bir Armağanı Olur
                 </p>
             </div>
             
         </div>
+
     </div>
   )
 }
--- src/Pages/ThirdPage.js
+++ src/Pages/ThirdPage.js
@@ -1,25 +1,60 @@
 import React from 'react'
+import { useState, useRef, useEffect } from 'react';
 import PartOneImg from "../Images/mentoring2.png";
 import PartTwoImg from "../Images/mentoring4.png";
 import '../Style/ThirdPage.css'
 
 function ThirdPage() {
+    const targetRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false)
+
+    const callbackFunction = entries =>{
+        const [entry] = entries
+        setIsVisible(entry.isIntersecting)
+        if (isVisible) {
+            document.getElementById("thirdpage-partOneText").classList.add("active")
+            document.getElementById("thirdpage-partOneImg").classList.add("active")
+            document.getElementById("thirdpage-partTwoImg").classList.add("active")
+            document.getElementById("thirdpage-partTwoText").classList.add("active")
+            console.log(entry);
+        }
+    }
+
+    const options = {
+        root:null,
+        rootMargin:"0px",
+        threshold:0.5
+    }
+
+    useEffect(() => {
+      
+        const observer = new IntersectionObserver(callbackFunction, options)
+        if (targetRef.current) {
+            observer.observe(targetRef.current)
+        }
+      return () => {
+        if (targetRef.current) {
+            observer.unobserve(targetRef.current)
+        }
+      }
+    }, [targetRef, options])
+
   return (
-    <div className='ThirdPage'>
+    <div className='ThirdPage' ref={targetRef}>
         <div className="part-one">
-            <div className="text">
+            <div className="text" id='thirdpage-partOneText'>
                 <p>Farklı Kişilerle Mentorluk Yaparak Gelişirsin</p>
             </div>
-            <div className="img">
+            <div className="img" id='thirdpage-partOneImg'>
                 <img src={PartOneImg} alt="" />
             </div>
         </div>
         <div className="part-two">
-            <div className="img">
+            <div className="img" id='thirdpage-partTwoImg'>
                 <img src={PartTwoImg} alt="" />
             </div>
 
-            <div className="text">
+            <div className="text" id='thirdpage-partTwoText'>
                 <p>Mentilerin Gelişim Gösterirken Kendi Kör Noktalarını Öğrenmelerine Benzer Şekilde, 
                     Sen de Mentor Olarak Bir Öğrenme Sürecinde Olursun.
                     Bu, Son Derece Kıymetlidir
